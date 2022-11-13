e00aae8d1ff5f7007bf88e8ebbb575b043c91228
maiminhnhat <kamenriderbon@gmail.com>
2022-11-13 10:18:27 +0700

    commit delete white spacce

--- E01/bai04.js
+++ E01/bai04.js
@@ -1,8 +1,7 @@
-let readline = require("readline-sync"); // Khai báo thư 
+let readline = require("readline-sync"); // Khai báo thư
 
+let a = Number(readline.question("Nhập gía trị độ F cần chuyển đổi: "));
 
-let a = Number(readline.question("Nhập gía trị độ F cần chuyển đổi: "))
+let c = (5 / 9) * (a - 32);
 
-let c = 5/9*(a - 32);
-
-console.log("gia tri cua do C:", c)
+console.log("gia tri cua do C:", c);
